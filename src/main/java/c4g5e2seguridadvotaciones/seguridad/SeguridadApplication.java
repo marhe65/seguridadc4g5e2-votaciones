1c87e2caecc766e59c3b97b799b493f6b8b5d9d4
Martin Luna <mhluna6519@gmail.com>
2022-11-07 12:37:02 -0500

    cargando poryecto a Git Hub

---
+++ src/main/java/c4g5e2seguridadvotaciones/seguridad/SeguridadApplication.java
@@ -0,0 +1,13 @@
+package c4g5e2seguridadvotaciones.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c4g5e2seguridadvotaciones/seguridad/SeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package c4g5e2seguridadvotaciones.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
