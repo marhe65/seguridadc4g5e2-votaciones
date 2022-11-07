a500c10ca222b99bda11c6111da67d5a2b716ce5
Martin Luna <mhluna6519@gmail.com>
2022-11-07 18:35:11 -0500

    Creaciòn package Repositorios y sus interfaces

---
+++ src/main/java/Repositorios/RepositorioUsuario.java
@@ -0,0 +1,10 @@
+package Repositorios;
+
+import Modelos.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RepositorioUsuario extends MongoRepository<Usuario,String> {
+    @Query("{'correo': ?0}")
+    public Usuario getUserByEmail(String correo);
+}
